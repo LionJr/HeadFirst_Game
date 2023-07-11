c1d0f87bb7dc39313b696f293a3fc09f9f16b671
LionJr <arslan.enigma.man@gmail.com>
2023-07-11 12:14:41 +0500

    Now we add emojis and remove smiles, then we make our game mouse click sensitive

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -27,11 +27,35 @@ namespace HeadFirst
             SetUpGame();
         }
 
+    TextBlock lastTextBlockClicked;
+    bool findingMatch = false;
+
+    private void TextBlock_MouseDown(object sender, MouseButtonEventArgs e)
+    {
+      TextBlock textBlock = sender as TextBlock;
+      if(findingMatch == false)
+      {
+        textBlock.Visibility = Visibility.Hidden;
+        lastTextBlockClicked = textBlock;
+        findingMatch = true;
+      }
+      else if(textBlock.Text == lastTextBlockClicked.Text)
+      {
+        textBlock.Visibility = Visibility.Hidden;
+        findingMatch = false;
+      }
+      else
+      {
+        lastTextBlockClicked.Visibility = Visibility.Visible;
+        findingMatch = false;
+      }
+    }
+
     private void SetUpGame()
     {
       List<string> emojiList = new List<string>()
       {
-        ":)", ":)",";)",";)",":-)",":-)",";-)",";-)",":--)",":--)",";--)",";--)",":(",":(",";(",";(" 
+        "😍", "😍","😄","😄","😅","😅","😈","😈","😎","😎","😝","😝","😠","😠","😤","😤" 
       };
       Random random = new Random();
       foreach (TextBlock textBlock in mainGrid.Children.OfType<TextBlock>())
