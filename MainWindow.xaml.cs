2a186351c4a83393df1143eb5d379da1584a4fd5
LionJr <arslan.enigma.man@gmail.com>
2023-07-11 13:03:05 +0500

    Now we have completed our game!!!

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -15,17 +15,41 @@ using System.Windows.Shapes;
 
 namespace HeadFirst
 {
+  using System.Windows.Threading;
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+      DispatcherTimer timer = new DispatcherTimer();
+      int seconds;
+      int matchesFound;
         public MainWindow()
         {
             InitializeComponent();
 
+            timer.Interval = TimeSpan.FromSeconds(.1);
+            timer.Tick += Timer_Tick;
+
             SetUpGame();
         }
+    private void Timer_Tick(object sender, EventArgs e)
+    {
+      seconds++;
+      timeTextBlock.Text = (seconds/10F).ToString("0.0s");
+      if(matchesFound==8)
+      {
+        timer.Stop();
+        timeTextBlock.Text = timeTextBlock.Text + " - Play again?";
+      }
+    }
+    private void TimeTextBlock_MouseDown(object sender, MouseButtonEventArgs e)
+    {
+      if(matchesFound==8)
+      {
+        SetUpGame();
+      }
+    }
 
     TextBlock lastTextBlockClicked;
     bool findingMatch = false;
@@ -41,6 +65,7 @@ namespace HeadFirst
       }
       else if(textBlock.Text == lastTextBlockClicked.Text)
       {
+        matchesFound++;
         textBlock.Visibility = Visibility.Hidden;
         findingMatch = false;
       }
@@ -60,10 +85,17 @@ namespace HeadFirst
       Random random = new Random();
       foreach (TextBlock textBlock in mainGrid.Children.OfType<TextBlock>())
       {
-        int index = random.Next(emojiList.Count);
-        string nextEmoji = emojiList[index];
-        textBlock.Text = nextEmoji;
-        emojiList.RemoveAt(index);
+        if(textBlock.Name != "timeTextBlock")
+        { 
+          textBlock.Visibility = Visibility.Visible;
+          int index = random.Next(emojiList.Count);
+          string nextEmoji = emojiList[index];
+          textBlock.Text = nextEmoji;
+          emojiList.RemoveAt(index);
+        }
+        timer.Start();
+        seconds = 0;
+        matchesFound = 0;
       }
     }
   }
